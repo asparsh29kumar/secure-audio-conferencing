cd4ce2507f947a71576979f4030ba42a3504e276
asparsh29kumar <asparsh29kumar@gmail.com>
2017-04-02 16:41:21 +0530

    Merged

--- src/unit-test/wav_merge/merge.py
+++ src/unit-test/wav_merge/merge.py
@@ -1,8 +1,9 @@
 from pydub import AudioSegment
+from pydub.playback import play
 
 sound1 = AudioSegment.from_file("./file1.wav")
 sound2 = AudioSegment.from_file("./file2.wav")
 
 combined = sound1.overlay(sound2)
-
+play(combined)
 # combined.export("./combined.wav", format='wav')
